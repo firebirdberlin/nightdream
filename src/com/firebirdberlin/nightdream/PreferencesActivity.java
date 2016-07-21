fed42ba5abbc88ebdd764c956b8520dc3cfb622a
Stefan Fruhner <stefan.fruhner@googlemail.com>
2016-07-21 20:48:34 +0200

    Re-add the old PreferenceActivity for compatability with an api level less or equal 10

--- src/com/firebirdberlin/nightdream/PreferencesActivity.java
+++ src/com/firebirdberlin/nightdream/PreferencesActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.os.Build;
 
 public class PreferencesActivity extends PreferenceActivity {
 
@@ -17,7 +18,11 @@ public class PreferencesActivity extends PreferenceActivity {
     }
 
     public static void start(Context context) {
-        Intent myIntent = new Intent(context, PreferencesActivity.class);
-        context.startActivity(myIntent);
+        if (Build.VERSION.SDK_INT > 10) {
+            Intent myIntent = new Intent(context, PreferencesActivity.class);
+            context.startActivity(myIntent);
+        } else {
+            PreferencesActivityv9.start(context);
+        }
     }
 }
