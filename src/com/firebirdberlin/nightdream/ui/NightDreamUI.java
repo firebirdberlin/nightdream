4867b9067b9de66aa104bbbac69ec72daa3de335
Stefan Fruhner <stefan.fruhner@googlemail.com>
2016-11-08 17:59:01 +0100

    Completely hide the system UI when the app resumes.

--- src/com/firebirdberlin/nightdream/ui/NightDreamUI.java
+++ src/com/firebirdberlin/nightdream/ui/NightDreamUI.java
@@ -164,6 +164,7 @@ public class NightDreamUI {
         removeCallbacks(hideAlarmClock);
         handler.postDelayed(hideAlarmClock, 20000);
 
+        hideSystemUI();
         settings.reload();
         updateWeatherData();
 
@@ -531,13 +532,26 @@ public class NightDreamUI {
         layout.screenBrightness = value;
         layout.buttonBrightness = 0.f;
         window.setAttributes(layout);
-        hideSoftButtons();
+        fadeSoftButtons();
     }
 
-    private void hideSoftButtons() {
-        if (Build.VERSION.SDK_INT >= 14){
+    private void fadeSoftButtons() {
+        if (Build.VERSION.SDK_INT >= 14 && Build.VERSION.SDK_INT < 19){
             View decorView = window.getDecorView();
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+        }
+    }
+
+    private void hideSystemUI() {
+        if (Build.VERSION.SDK_INT >= 19){
+            View decorView = window.getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     }
 
