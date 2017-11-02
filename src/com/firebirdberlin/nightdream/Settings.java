85b7b3d24dccbb8223dd63f6fa6636c9f1afad85
Stefan Fruhner <stefan.fruhner@gmail.com>
2017-11-02 22:52:38 +0100

    Set force auto rotation to true by default

--- src/com/firebirdberlin/nightdream/Settings.java
+++ src/com/firebirdberlin/nightdream/Settings.java
@@ -149,7 +149,7 @@ public class Settings {
         autostartTimeRangeStart = settings.getLong("autostart_time_range_start", -1L); // deprecated
         autostartTimeRangeEnd = settings.getLong("autostart_time_range_end", -1L); // deprecated
         background_mode = Integer.parseInt(settings.getString("backgroundMode", "1"));
-        force_auto_rotation = settings.getBoolean("force_auto_rotation", false);
+        force_auto_rotation = settings.getBoolean("force_auto_rotation", true);
         handle_power = settings.getBoolean("handle_power", false);
         handle_power_disconnection = settings.getBoolean("handle_power_disconnection", false);
         handle_power_desk = settings.getBoolean("handle_power_desk", false);
