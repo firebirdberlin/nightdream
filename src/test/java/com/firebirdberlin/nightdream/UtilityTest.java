5f66b417939d295c4bd82a458e20d39d5dbba71f
Stefan Fruhner <stefan.fruhner@gmail.com>
2017-11-03 10:01:48 +0100

    Add a test for Utility

---
+++ src/test/java/com/firebirdberlin/nightdream/UtilityTest.java
@@ -0,0 +1,34 @@
+package com.firebirdberlin.nightdream;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * Created by stefan on 29.10.17.
+ */
+public class UtilityTest extends TestCase {
+    public void testGetWeekdayStringsforLocaleDe() throws Exception {
+        String[] weekdays = Utility.getWeekdayStringsForLocale(new Locale("de"));
+
+        for (int i = 0; i < weekdays.length; i++) {
+            System.out.println(weekdays[i]);
+        }
+
+        String[] expected = {"", "S", "M", "D", "M", "D", "F", "S"};
+        assert 7 == weekdays.length;
+        Assert.assertArrayEquals(expected, weekdays);
+    }
+
+    public void testGetWeekdayStringsforLocaleUs() throws Exception {
+        String[] weekdays = Utility.getWeekdayStringsForLocale(new Locale("us"));
+        String[] expected = {"", "S", "M", "T", "W", "T", "F", "S"};
+        assert 7 == weekdays.length;
+        Assert.assertArrayEquals(expected, weekdays);
+    }
+
+}
