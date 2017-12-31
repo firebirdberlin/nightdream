d76d2916486e156d26a051967b6b78e964cce07f
Martin Poschmann <martinposchmann@gmx.de>
2017-12-31 01:18:25 +0100

    use refactoring dialog class in a dialog fragment

--- src/com/firebirdberlin/nightdream/ui/RadioStreamDialogFragment.java
+++ src/com/firebirdberlin/nightdream/ui/RadioStreamDialogFragment.java
@@ -5,13 +5,20 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 import com.firebirdberlin.nightdream.R;
+import com.firebirdberlin.radiostreamapi.RadioStreamPreference;
+import com.firebirdberlin.radiostreamapi.models.RadioStation;
 
 public class RadioStreamDialogFragment extends DialogFragment {
+
+    private RadioStreamDialog radioStreamDialog;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
+        /*
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage("Hallo")
                 .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
@@ -26,6 +33,37 @@ public class RadioStreamDialogFragment extends DialogFragment {
                 });
         // Create the AlertDialog object and return it
         return builder.create();
+        */
+
+        // TODO get current station
+        RadioStation station = null;
+
+        final RadioStreamDialogListener dialogListener = new RadioStreamDialogListener() {
+            public void onRadioStreamSelected(RadioStation station) {
+                //TODO save station to some preset (json)
+            }
+        };
+
+        radioStreamDialog = new RadioStreamDialog(getActivity(), station);
+        View view = radioStreamDialog.createDialogView(dialogListener);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.DialogTheme);
+
+        builder.setTitle(R.string.radio_stream)
+                .setView(view)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+
     }
 
 }
