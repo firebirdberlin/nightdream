31162a03bc77f7bef99c26162d5e5f91859e93fd
Martin Poschmann <martinposchmann@gmx.de>
2018-03-19 15:43:23 +0100

    fixed -0°C

--- src/com/firebirdberlin/nightdream/ui/WeatherLayout.java
+++ src/com/firebirdberlin/nightdream/ui/WeatherLayout.java
@@ -197,11 +197,11 @@ public class WeatherLayout extends LinearLayout {
     private String formatTemperatureText(WeatherEntry entry) {
         switch (temperatureUnit) {
             case WeatherEntry.CELSIUS:
-                return String.format("%.0f°C", toDegreesCelcius(entry.temperature));
+                return String.format("%d°C", Math.round(toDegreesCelcius(entry.temperature)));
             case WeatherEntry.FAHRENHEIT:
-                return String.format("%.0f°F", toDegreesFahrenheit(entry.temperature));
+                return String.format("%d°F", Math.round(toDegreesFahrenheit(entry.temperature)));
             default:
-                return String.format("%.0fK", entry.temperature);
+                return String.format("%d K", Math.round(entry.temperature));
         }
     }
     private double toDegreesCelcius(double kelvin) {
