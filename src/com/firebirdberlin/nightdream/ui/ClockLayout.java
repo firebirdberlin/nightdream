5f3d3c1c10e75df92f9df90638b1a85e6ffe7d1b
Stefan Fruhner <stefan.fruhner@gmail.com>
2019-11-24 10:00:21 +0100

    Bump version 262

--- src/com/firebirdberlin/nightdream/ui/ClockLayout.java
+++ src/com/firebirdberlin/nightdream/ui/ClockLayout.java
@@ -166,7 +166,8 @@ public class ClockLayout extends LinearLayout {
             BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
             view.getPaint().setShader(shader);
         }
-        view.setLayerType((glowRadius > 24) ? LAYER_TYPE_SOFTWARE : LAYER_TYPE_HARDWARE, null);
+        //view.setLayerType((glowRadius > 24) ? LAYER_TYPE_SOFTWARE : LAYER_TYPE_HARDWARE, null);
+        view.setLayerType(LAYER_TYPE_SOFTWARE, null);
     }
 
     public void setSecondaryColor(int color) {
