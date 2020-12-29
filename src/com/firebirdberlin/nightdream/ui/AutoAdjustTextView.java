2cfeee03428194c7e4ccc8386a13bec717da35db
Stefan Fruhner <stefan.fruhner@gmail.com>
2020-12-29 20:49:10 +0100

    Update the new digital clock layout

--- src/com/firebirdberlin/nightdream/ui/AutoAdjustTextView.java
+++ src/com/firebirdberlin/nightdream/ui/AutoAdjustTextView.java
@@ -5,7 +5,6 @@ import android.content.res.TypedArray;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Typeface;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
