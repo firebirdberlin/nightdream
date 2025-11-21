70dba69522270afb74eda4bc85ea8c466e207ca8
Stefan Fruhner <stefan.fruhner@gmail.com>
2025-11-21 17:23:43 +0100

    Remove implementation 'com.prof18.rssparser:rssparser:6.1.1' from dependencies

--- src/com/firebirdberlin/nightdream/services/RSSParserService.java
+++ src/com/firebirdberlin/nightdream/services/RSSParserService.java
@@ -2,6 +2,7 @@ package com.firebirdberlin.nightdream.services;
 
 import android.content.Context;
 import android.util.Log;
+import android.util.Xml;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
@@ -11,18 +12,20 @@ import androidx.work.WorkerParameters;
 import com.firebirdberlin.nightdream.R;
 import com.firebirdberlin.nightdream.Settings;
 import com.firebirdberlin.nightdream.models.RssFeedItem;
-import com.prof18.rssparser.RssParser;
-import com.prof18.rssparser.RssParserBuilder;
-import com.prof18.rssparser.model.RssChannel;
-import com.prof18.rssparser.model.RssItem;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 public class RSSParserService extends Worker {
     private static final String TAG = "RSSParserService";
     private String urlString = "https://www.tagesschau.de/xml/rss2/";
+//    private String urlString = "https://www.rbb24.de/index.xml/feed=rss.xml";
     public static MutableLiveData<List<RssFeedItem>> articleListLive = new MutableLiveData<>();
 
     public RSSParserService(@NonNull Context context, @NonNull WorkerParameters params) {
@@ -36,7 +39,6 @@ public class RSSParserService extends Worker {
 
         Settings settings = new Settings(getApplicationContext());
         String url = settings.rssURL;
-
         if (url.isEmpty()) {
             List<RssFeedItem> rssItems = new ArrayList<>();
             RssFeedItem rssItem = new RssFeedItem(
@@ -49,25 +51,16 @@ public class RSSParserService extends Worker {
             return Result.success();
         }
 
-        articleListLive.postValue(fetchRssItems(url));
+        articleListLive.postValue(fetchRssFeedItems(url));
 
         return Result.success();
     }
 
-    private List<RssFeedItem> fetchRssItems(String url) {
-        RssParser parser = new RssParserBuilder().build();
-        CompletableFuture<RssChannel> future = CoroutineBridge.INSTANCE.parseFeed(parser, url);
-
+    private List<RssFeedItem> fetchRssFeedItems(String urlString) {
         try {
-            RssChannel channel = future.get();
-            if (channel != null) {
-                List<RssFeedItem> feedItems = new ArrayList<>();
-                for (RssItem item : channel.getItems()) {
-                    feedItems.add(new RssFeedItem(item.getTitle(), item.getLink(), item.getPubDate()));
-                }
-                return feedItems;
-            }
-        } catch (Exception e) {
+            InputStream stream = new URL(urlString).openStream();
+            return parse(stream);
+        } catch (IOException | XmlPullParserException e) {
             Log.e(TAG, "Error parsing RSS feed", e);
             List<RssFeedItem> rssItems = new ArrayList<>();
             RssFeedItem rssItem = new RssFeedItem(
@@ -78,6 +71,120 @@ public class RSSParserService extends Worker {
             rssItems.add(rssItem);
             return rssItems;
         }
-        return new ArrayList<>();
+    }
+
+    private List<RssFeedItem> parse(InputStream in) throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+            parser.nextTag();
+            return readFeed(parser);
+        } finally {
+            in.close();
+        }
+    }
+
+    private List<RssFeedItem> readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List<RssFeedItem> entries = new ArrayList<>();
+
+        parser.require(XmlPullParser.START_TAG, null, "rss");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            // Starts by looking for the entry tag
+            if (name.equals("channel")) {
+                while (parser.next() != XmlPullParser.END_TAG) {
+                    if (parser.getEventType() != XmlPullParser.START_TAG) {
+                        continue;
+                    }
+                    String channelName = parser.getName();
+                    if (channelName.equals("item")) {
+                        entries.add(readEntry(parser));
+                    } else {
+                        skip(parser);
+                    }
+                }
+            } else {
+                skip(parser);
+            }
+        }
+        return entries;
+    }
+
+
+    private RssFeedItem readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, null, "item");
+        String title = null;
+        String link = null;
+        String pubDate = null;
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("title")) {
+                title = readTitle(parser);
+            } else if (name.equals("link")) {
+                link = readLink(parser);
+            } else if (name.equals("pubDate")) {
+                pubDate = readPubDate(parser);
+            } else {
+                skip(parser);
+            }
+        }
+        return new RssFeedItem(title, link, pubDate);
+    }
+
+    // Processes title tags in the feed.
+    private String readTitle(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, null, "title");
+        String title = readText(parser);
+        parser.require(XmlPullParser.END_TAG, null, "title");
+        return title;
+    }
+
+    // Processes link tags in the feed.
+    private String readLink(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, null, "link");
+        String link = readText(parser);
+        parser.require(XmlPullParser.END_TAG, null, "link");
+        return link;
+    }
+
+    private String readPubDate(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, null, "pubDate");
+        String pubDate = readText(parser);
+        parser.require(XmlPullParser.END_TAG, null, "pubDate");
+        return pubDate;
+    }
+
+    // For the tags title and summary, extracts their text values.
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
     }
 }
