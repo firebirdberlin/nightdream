7f339ff27c6b7ecac1d77f83181de66708f9d06b
mastermichi <mastermichi@freenet.de>
2026-02-09 20:02:43 +0100

    Removed unused import

--- app/src/main/java/com/firebirdberlin/nightdream/services/ImageCopyService.java
+++ app/src/main/java/com/firebirdberlin/nightdream/services/ImageCopyService.java
@@ -3,7 +3,6 @@ package com.firebirdberlin.nightdream.services;
 import android.app.ForegroundServiceStartNotAllowedException;
 
 import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.content.pm.ServiceInfo;
